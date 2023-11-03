b8fce3427c8356096dfb7993756bddf088232000
archmvv <62836739+archmvv@users.noreply.github.com>
2023-11-03 10:14:52 +0200

    BubbleSort algorithm implementation

---
+++ BubbleSort.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = rand.nextInt(1000);
+        }
+
+        System.out.println("Before:");
+        printArray(numbers);
+
+        bubbleSort(numbers);
+
+        System.out.println("\nAfter:");
+        printArray(numbers);
+    }
+
+    private static void bubbleSort(int[] numbers) {
+        boolean swappedSomthing = true;
+
+        while (swappedSomthing) {
+            swappedSomthing = false;
+            for (int i = 0; i < numbers.length - 1; i++) {
+                if (numbers[i] > numbers[i + 1]) {
+                    swappedSomthing = true;
+                    int temp = numbers[i];
+                    numbers[i] = numbers[i + 1];
+                    numbers[i + 1] = temp;
+                }
+            }
+        }
+    }
+
+    private static void printArray(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.printf("%d ", numbers[i]);
+        }
+    }
+}
